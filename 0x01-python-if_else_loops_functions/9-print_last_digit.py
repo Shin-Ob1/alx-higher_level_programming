cf5e7b61c785002a665e3251721d5bad38070af5
Obinna Ukwa <obikelv@gmail.com>
2023-06-06 09:59:58 -0700

    last digit debug

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number < 0:
-        number = number % -10
+        print("{:d}".format(-(number % -10)), end='')
+        return(-(number % -10))
     else:
-        number = number % 10
-
-    print("{:d}".format(number), end="")
-    return number
+        print("{:d}".format(number % 10), end='')
+        return(number % 10)
