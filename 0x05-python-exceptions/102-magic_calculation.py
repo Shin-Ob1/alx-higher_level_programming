e06ea8becbd383c98dd7ae9f95810956fbcaf6b2
Obinna Ukwa <obikelv@gmail.com>
2023-06-26 11:48:14 -0700

    python bytecode

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    num = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            else:
+                num += (a ** b) / i
+        except Exception:
+            num = b + a
+            break
+    return (num)
