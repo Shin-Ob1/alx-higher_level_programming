6897d1c04faa6a9a7ad064039428f1000fed22c2
Obinna Ukwa <obikelv@gmail.com>
2023-07-10 21:05:45 -0700

    checks if inheritance of a class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Contains of a single function"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+        Check if the object is an instance of a class
+    """
+    return isinstance(obj, a_class)
