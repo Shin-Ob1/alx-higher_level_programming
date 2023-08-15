aa65220e1f619151b43d70cc804be352533d0b60
Obinna Ukwa <obikelv@gmail.com>
2023-08-15 16:29:55 -0700

    top 3 hot cities

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,4 @@
+-- script to display the top 3 city temperature during July and August
+SELECT city, AVG(value) AS avg_temp FROM temperatures
+WHERE month BETWEEN 7 AND 8
+GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
