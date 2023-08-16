4041605e088a12a6cf81129f9593e701a245479a
Obinna Ukwa <obikelv@gmail.com>
2023-08-16 11:07:56 -0700

    import comedy from db dump from hbtn_0d_tvshows

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,10 @@
+-- lists all shows, and genres linked to that show, from hbtn_0d_tvshows
+
+SELECT tv_shows.title AS title 
+FROM tv_shows
+INNER JOIN tv_show_genres 
+ON tv_show_genres.show_id = tv_shows.id
+INNER JOIN tv_genres 
+ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = "Comedy"
+ORDER BY title ASC;
