dbf785cd49bc08fae1b8643da3ff8d2870badc7a
ShinOb1 <obikelv@gmail.com>
2023-09-12 12:18:47 -0700

    return number of occurences

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+    return list.reduce(function (count, current) {
+        if (current === searchElement) {
+            return count + 1;
+        } else {
+            return count;
+        }
+    }, 0);
+};
