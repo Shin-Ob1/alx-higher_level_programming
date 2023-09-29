c52c0a476a0d6a95586a8c0d548740b97b19aa6e
ShinOb1 <obikelv@gmail.com>
2023-09-29 12:09:36 -0700

    get github credentials

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""list 10 most recent commits on a GitHub repo"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
+
+    re = requests.get(url)
+    commits = re.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
